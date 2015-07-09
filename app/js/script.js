11356f1fad87700912d99fde65399ab01d6be9e8
skl92 <stanleykliu92@gmail.com>
2015-07-08 21:25:52 -0700

    add navbar and single gig pages

--- app/js/script.js
+++ app/js/script.js
@@ -21,8 +21,15 @@ mob.config(function($stateProvider, $urlRouterProvider) {
 .controller('FeedCtrl', function() {
 
 })
-.controller('GigCtrl', function() {
+.controller('GigCtrl', function($scope) {
   console.log('working');
+  $scope.service = {
+    name: "Guitar Lessons",
+    poster: "Javier",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam repudiandae eaque, facilis quasi, vitae dolore quam dicta. Ratione, doloribus, odit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis facere, excepturi minus quos! Ipsum sunt sequi, ratione hic aliquid corrupti esse illo, alias voluptate aliquam quasi, consequatur, labore optio excepturi?",
+    charity: "Red Cross",
+    price: "$500"
+  }
 })
 .controller('CreateCtrl', function() {
 
