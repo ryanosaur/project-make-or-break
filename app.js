1be76f7235c40989f70b5e8f56cc7e5ef4c3dde6
Ben Halverson <benhalverson33@gmail.com>
2015-07-08 18:51:58 -0700

    fixed serving static files

--- app.js
+++ app.js
@@ -21,11 +21,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
 
-app.get('/', function(req, res) {
-  res.sendfile(app_dir + 'index.html');
-});
 
 app.use('/api', routes);
 app.use('/users', users);
