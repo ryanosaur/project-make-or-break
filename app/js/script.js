ddbf6b44e467035411098c3b726efee8b4d7f8ff
skl92 <stanleykliu92@gmail.com>
2015-07-08 20:06:41 -0700

    ui router

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,20 @@
 var mob = angular.module("mob", ['ui.router']);
 mob.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/gigs');
+  $urlRouterProvider.otherwise('/home');
   $stateProvider
     .state('gigs', {
-
+      url: "/home",
+      templateUrl: "views/feed.html",
+      controller: "FeedCtrl"
     })
     .state('gig', {
-
+      url: "/gig/:id",
+      templateUrl: "views/onegig.html",
+      controller: "GigCtrl"
     })
     .state('create' {
-
-    })
+      url: "/create",
+      templateUrl: "views/create.html",
+      controller: "CreateCtrl"
+    });
 })
